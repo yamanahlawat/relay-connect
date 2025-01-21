a739d9613b5d98ee5ab0a12ab65989736194453d
Yaman Ahlawat <yaman@outlook.com>
2025-01-21 08:24:55 +0530

    refactor: moved chat related components, hooks and layouts to modules/chat

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -36,4 +36,4 @@ const ResizableHandle = ({
   </ResizablePrimitive.PanelResizeHandle>
 );
 
-export { ResizablePanelGroup, ResizablePanel, ResizableHandle };
+export { ResizableHandle, ResizablePanel, ResizablePanelGroup };
